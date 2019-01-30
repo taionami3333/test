57d82d47da537c1489255092105d16fe896408cd
internous-k <kota.minamiya@gmail.com>
2019-01-30 12:48:29 +0900

    java added

---
+++ wroteplot/src/com/internousdev/wroteplus/action/HomeAction.java
@@ -0,0 +1,9 @@
+package com.internousdev.wroteplus.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport{
+	public String execute(){
+		return SUCCESS;
+	}
+}
---
+++ wroteplot/src/com/internousdev/wroteplus/action/UserCreateAction.java
@@ -0,0 +1,9 @@
+package com.internousdev.wroteplus.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserCreateAction extends ActionSupport{
+	public String execute(){
+		return SUCCESS;
+	}
+}
